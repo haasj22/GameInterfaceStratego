56307462b5bba93d03e6a4c4a09f2508dbb4f714
Kavya Mandla <mandla21@up.edu>
2019-10-09 13:36:17 -0700

    added gameState class

--- app/src/main/java/com/example/myapplication/StrategoGameState.java
+++ app/src/main/java/com/example/myapplication/StrategoGameState.java
@@ -1,4 +1,12 @@
 package com.example.myapplication;
 
+/**
+ * The State of the game.
+ * Includes values for pieces,
+ *
+ * @author Kavya Mandla
+ * @author
+ */
+
 public class StrategoGameState {
 }
