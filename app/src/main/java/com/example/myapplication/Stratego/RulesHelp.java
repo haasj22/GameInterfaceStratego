f58d9ddba21f6836d822cbd6a062e3585680165d
Kavya Mandla <mandla21@up.edu>
2019-10-29 16:20:11 -0700

    starting to implement menu drop down and rules screens

---
+++ app/src/main/java/com/example/myapplication/Stratego/RulesHelp.java
@@ -0,0 +1,20 @@
+package com.example.myapplication.Stratego;
+
+import android.view.View;
+
+import com.example.myapplication.Game.Game;
+import com.example.myapplication.Game.GameMainActivity;
+import com.example.myapplication.Game.GamePlayer;
+
+public class RulesHelp extends XMLObject{
+    private final GameMainActivity activity;
+    private GamePlayer player;
+    private Game game;
+
+    public RulesHelp(View v, GamePlayer player, Game game, GameMainActivity activity){
+        super(v);
+        this.player = player;
+        this.game = game;
+        this.get
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Stratego/rulesButtons.java
@@ -0,0 +1,5 @@
+package com.example.myapplication.Stratego;
+
+public enum rulesButtons {
+    NEXT, PREVIOUS, EXIT
+}
