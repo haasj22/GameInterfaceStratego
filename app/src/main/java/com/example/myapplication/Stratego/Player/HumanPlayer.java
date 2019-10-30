0e8de892046bbb06a9ee77e60e7581f3068755ff
Kavya Mandla <mandla21@up.edu>
2019-10-29 18:06:53 -0700

    added empty drawable //error

--- app/src/main/java/com/example/myapplication/Stratego/Player/HumanPlayer.java
+++ app/src/main/java/com/example/myapplication/Stratego/Player/HumanPlayer.java
@@ -6,25 +6,52 @@ import android.widget.SeekBar;
 
 import com.example.myapplication.Game.GameMainActivity;
 import com.example.myapplication.Game.GameHumanPlayer;
-/**
-public class HumanPlayer extends GameHumanPlayer implements View.OnClickListener,
-        SeekBar.OnSeekBarChangeListener{
+import com.example.myapplication.Game.infoMsg.GameInfo;
+import com.example.myapplication.R;
+import com.example.myapplication.StandardGameBoard;
+import com.example.myapplication.Stratego.GameState.StrategoGameState;
+import com.example.myapplication.Stratego.RulesHelp;
+
+public class HumanPlayer extends GameHumanPlayer {
+
+    private RulesHelp rulesHelpButton;
+    private GameMainActivity activity;
+    private StrategoGameState state;
+
+
+    private StandardGameBoard gameBoard;
 
     private Button startButton;
     private Button forfeitButton;
 
-    public HumanPlayer(String name){
+    public HumanPlayer(String name) {
         super(name);
     }
-
-    /**
-    public receiveInfo(GameInfo info){
-        // receives info from game state
+    public View getTopView(){
+        return activity.findViewById(R.id.topGUI);
     }
-     **/
-    /**
+
+
+
+
+    public void receiveInfo(GameInfo info){
+        if (info instanceof StrategoGameState){
+            if(rulesHelpButton.getGame() == null){
+
+
+            }
+        }
+    }
+
+
     public void setAsGui(GameMainActivity activity){
-        // sets gui with most current information
+        this.activity = activity;
+        activity.setContentView(R.layout.stratego_rules);
+
+        rulesHelpButton = new RulesHelp(this.activity.findViewById(R.id.menuButton),
+                this, this.game, this.activity);
+
+
     }
     private void updateGui(){
         // updates gui after actions are played
