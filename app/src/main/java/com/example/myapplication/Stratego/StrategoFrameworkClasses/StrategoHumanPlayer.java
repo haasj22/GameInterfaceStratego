dc3fa4fb7854cd74c2fa5319f883fc342eb86f73
johnhaas <haasj22@up.edu>
2019-11-03 11:40:52 -0800

    finished strategoMainActivity. Does Not compile yet

---
+++ app/src/main/java/com/example/myapplication/Stratego/StrategoFrameworkClasses/StrategoHumanPlayer.java
@@ -0,0 +1,4 @@
+package com.example.myapplication.Stratego.StrategoFrameworkClasses;
+
+public class StrategoHumanPlayer {
+}
---
+++ app/src/main/java/com/example/myapplication/Stratego/StrategoFrameworkClasses/StrategoMainActivity.java
@@ -0,0 +1,77 @@
+package com.example.myapplication.Stratego.StrategoFrameworkClasses;
+
+import com.example.myapplication.Game.GameMainActivity;
+import com.example.myapplication.Game.GamePlayer;
+import com.example.myapplication.Game.LocalGame;
+import com.example.myapplication.Game.gameConfiguration.GameConfig;
+import com.example.myapplication.Game.gameConfiguration.GamePlayerType;
+import com.example.myapplication.R;
+import com.example.myapplication.Stratego.GameState.DumbComputerPlayer;
+import com.example.myapplication.Stratego.GameState.SmartComputerPlayer;
+
+import java.util.ArrayList;
+
+public class StrategoMainActivity extends GameMainActivity {
+
+    //Tag for logging
+    private static final String TAG = "Stratego Main Activity";
+    public static final int PORT_NUMBER = 5213;
+
+    /**
+     * a stratego game for two players. The default is human vs. computer
+     *
+     * @return the default configuration for a stratego game
+     */
+    @Override
+    public GameConfig createDefaultConfig() {
+
+        //Define the allowed player types
+        ArrayList<GamePlayerType> playerTypes= new ArrayList<GamePlayerType>();
+
+        //human player's view
+        playerTypes.add(new GamePlayerType("Local Human Player (red-team)") {
+            @Override
+            public GamePlayer createPlayer(String name) {
+                return new StrategoHumanPlayer(name, R.layout.stratego_board);
+            }
+        });
+
+        //dumb computer player
+        playerTypes.add(new GamePlayerType("ComputerPlayer (dumb)") {
+            @Override
+            public GamePlayer createPlayer(String name) {
+                return new DumbComputerPlayer(name);
+            }
+        });
+
+        //smart computer player
+        playerTypes.add(new GamePlayerType("ComputerPlayer (smart)") {
+            @Override
+            public GamePlayer createPlayer(String name) {
+                return new SmartComputerPlayer(name);
+            }
+        });
+
+        //create a default two player configuration for stratego
+        GameConfig defaultConfig =
+                new GameConfig(playerTypes, 2, 2, "Stratego", PORT_NUMBER);
+
+        defaultConfig.addPlayer("Human", 0); //human player
+        defaultConfig.addPlayer("Computer", 1); //dumb computer player
+
+        //sets the initial configuration for the remote player
+        defaultConfig.setRemoteData("RemotePlayer", "ipCode", 0);
+
+        return defaultConfig;
+    }
+
+    /**
+     * createLocalGame
+     *
+     * Creates a new game that runs on the server tablet,
+     *
+     * @return a new, game-specific instance of the LocalGame class
+     */
+    @Override
+    public LocalGame createLocalGame() { return new StrategoLocalGame();}
+}
