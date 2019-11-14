c2d797e19674267c6c1e01af0f995e11ab4a00cd
Kavya Mandla <mandla21@up.edu>
2019-11-14 09:17:05 -0800

    fixed 9 and spy button in HumanPlayer

--- app/src/test/java/com/example/myapplication/CopyConstructorTest.java
+++ app/src/test/java/com/example/myapplication/CopyConstructorTest.java
@@ -1,4 +1,25 @@
 package com.example.myapplication;
 
+import static org.junit.Assert.*;
+import com.example.myapplication.Stratego.GameState.StrategoGameState;
+
+
+import org.junit.BeforeClass;
+
+/**
+ * Tests the copy constructor for every class in the game state
+ *
+ * Kavya Mandla
+ */
+
 public class CopyConstructorTest {
+
+    private StrategoGameState state;
+
+    @BeforeClass
+    public static void setup(){
+        GameStateUnitTest
+
+
+    }
 }
--- app/src/test/java/com/example/myapplication/GameStateUnitTest.java
+++ app/src/test/java/com/example/myapplication/GameStateUnitTest.java
@@ -16,7 +16,7 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class GameStateUnitTest {
     @Test
     public void canAddFlagToTheGame() {
         StrategoGameState testGameState = new StrategoGameState();
