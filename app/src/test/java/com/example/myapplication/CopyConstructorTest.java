6027924c8edde03d1b04e2583b402ff0fa56d97f
Jordan Ho <hojo21@up.edu>
2019-11-23 14:18:45 -0800

    unit tests for game state

--- app/src/test/java/com/example/myapplication/CopyConstructorTest.java
+++ app/src/test/java/com/example/myapplication/CopyConstructorTest.java
@@ -1,10 +1,14 @@
 package com.example.myapplication;
 
 import static org.junit.Assert.*;
+
+import com.example.myapplication.Stratego.GameState.Piece;
+import com.example.myapplication.Stratego.GameState.Rank;
 import com.example.myapplication.Stratego.GameState.StrategoGameState;
 
 
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 /**
  * Tests the copy constructor for every class in the game state
@@ -22,5 +26,72 @@ public class CopyConstructorTest {
 
 
     }
+
+    @Test
+    public void movePiece(){
+        StrategoGameState testGameState = new StrategoGameState();
+        testGameState.addPieceToGame(new Piece(testGameState.getCurrentTeamsTurn(), Rank.ONE),3,3);
+        testGameState.transitionPhases();
+        testGameState.tapOnSquare(3,3);
+        testGameState.tapOnSquare(4,4);
+        assertEquals(testGameState.getPieceAt(3,3),null);
+        assertEquals(Rank.ONE,testGameState.getPieceAt(4,4));
+    }
+
+    @Test
+    public void attackPiece(){
+        StrategoGameState testGameState = new StrategoGameState();
+        testGameState.addPieceToGame(new Piece(testGameState.getCurrentTeamsTurn(), Rank.EIGHT), 4,4);
+        testGameState.transitionPhases();
+        testGameState.addPieceToGame(new Piece(testGameState.getCurrentTeamsTurn(),Rank.TWO),4,3);
+        testGameState.transitionPhases();
+        testGameState.tapOnSquare(4,4);
+        testGameState.tapOnSquare(4,3);
+        assertEquals(Rank.TWO,testGameState.getPieceAt(4,3));
+    }
+
+    @Test
+    public void scoutAttack(){
+        StrategoGameState testGameState = new StrategoGameState();
+        testGameState.addPieceToGame(new Piece(testGameState.getCurrentTeamsTurn(),Rank.EIGHT),4,4);
+        testGameState.transitionPhases();
+        testGameState.addPieceToGame(new Piece(testGameState.getCurrentTeamsTurn(),Rank.SPY),4,3);
+        testGameState.transitionPhases();
+        testGameState.tapOnSquare(4,4);
+        testGameState.tapOnSquare(4,3);
+
+
+    }
+
+    @Test
+    public void spyAttack(){
+        StrategoGameState testGameState = new StrategoGameState();
+        testGameState.addPieceToGame(new Piece(testGameState.getCurrentTeamsTurn(),Rank.SPY),4,4);
+        testGameState.transitionPhases();
+        testGameState.addPieceToGame(new Piece(testGameState.getCurrentTeamsTurn(),Rank.ONE),4,3);
+        testGameState.transitionPhases();
+        testGameState.tapOnSquare(4,4);
+        testGameState.tapOnSquare(4,3);
+        assertEquals(Rank.SPY,testGameState.getPieceAt(4,3));
+        testGameState.transitionPhases();
+        testGameState.addPieceToGame(new Piece(testGameState.getCurrentTeamsTurn(),Rank.TWO),4,4);
+        testGameState.transitionPhases();
+        testGameState.tapOnSquare(4,3);
+        testGameState.tapOnSquare(4,4);
+        assertEquals(Rank.TWO,testGameState.getPieceAt(4,4));
+    }
+
+    @Test
+    public void attackBomb(){
+        StrategoGameState testGameState = new StrategoGameState();
+        testGameState.addPieceToGame(new Piece(testGameState.getCurrentTeamsTurn(),Rank.SIX),4,4);
+        testGameState.transitionPhases();
+        testGameState.addPieceToGame(new Piece(testGameState.getCurrentTeamsTurn(), Rank.BOMB),4,3);
+        testGameState.transitionPhases();
+        testGameState.tapOnSquare(4,4);
+        testGameState.tapOnSquare(4,3);
+        assertEquals(testGameState.getPieceAt(4,3),null);
+    }
+
 
 }
